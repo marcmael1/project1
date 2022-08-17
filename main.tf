94fc55e722113be4168c27e69d8023700ee12ef0
marcmael1 <marctchouanche@gmail.com>
2022-08-17 15:14:58 -0400

    Syntax update

--- main.tf
+++ main.tf
@@ -1,35 +1,30 @@
-locals {
-  public_cidr  = ["10.0.0.0/24", "10.0.1.0/24"]
-  private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
-}
-
 resource "aws_vpc" "demo_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
-    "Name" = "demo-vpc"
+    "Name" = var.env_code
   }
 }
 
 resource "aws_subnet" "public" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
   
   vpc_id     = aws_vpc.demo_vpc.id
-  cidr_block = local.public_cidr[count.index]
+  cidr_block = var.public_cidr[count.index]
 
   tags = {
-    Name = "public-subnet${count.index}"
+    Name = "${var.env_code}-public-subnet${count.index}"
   }
 }
 
 resource "aws_subnet" "private" {
-  count = length(local.private_cidr)
+  count = length(var.private_cidr)
  
   vpc_id     = aws_vpc.demo_vpc.id
-  cidr_block = local.private_cidr[count.index]
+  cidr_block = var.private_cidr[count.index]
 
   tags = {
-    Name = "private-subnet${count.index}"
+    Name = "${var.env_code}-private-subnet${count.index}"
   }
 }
 
@@ -37,29 +32,28 @@ resource "aws_internet_gateway" "demo-gw" {
   vpc_id = aws_vpc.demo_vpc.id
 
   tags = {
-    Name = "demo-gw"
+    Name = var.env_code
   }
 }
 
-
 resource "aws_eip" "nat" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
 
   vpc = true
 
   tags = {
-    "Name" = "demo-nat${count.index}"
+    "Name" = "${var.env_code}-demo-nat${count.index}"
   }
 }
 
 resource "aws_nat_gateway" "nat-gw" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
 
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-    "Name" = "demo-nat-gw${count.index}"
+    "Name" = "${var.env_code}-demo-nat-gw${count.index}"
   }
 }
 
@@ -72,12 +66,12 @@ resource "aws_route_table" "public" {
   }
 
   tags = {
-    "Name" = "demo-public-rt"
+    "Name" = "${var.env_code}-demo-public-rt"
   }
 }
 
 resource "aws_route_table" "private" {
-  count = length(local.private_cidr)
+  count = length(var.private_cidr)
 
   vpc_id = aws_vpc.demo_vpc.id
 
@@ -87,19 +81,19 @@ resource "aws_route_table" "private" {
   }
 
   tags = {
-    "Name" = "demo-private${count.index}"
+    "Name" = "${var.env_code}-demo-private${count.index}"
   }
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
 
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(local.private_cidr)
+  count = length(var.private_cidr)
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
