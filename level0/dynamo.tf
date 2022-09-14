e24fba1fa42ffdf4bb181d6aaf6c7c6497e05977
marcmael1 <marctchouanche@gmail.com>
2022-09-13 22:49:35 -0400

    configure remote backend

---
+++ level0/dynamo.tf
@@ -0,0 +1,11 @@
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 10
+  write_capacity = 10
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
