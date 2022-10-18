547ca35f8ebc4600199d822ce92b2abe5eb693ce
marcmael1 <marctchouanche@gmail.com>
2022-10-17 20:21:45 -0400

    created load balancer

--- level2/instance.tf
+++ level2/instance.tf
@@ -67,7 +67,7 @@ resource "aws_instance" "private" {
   vpc_security_group_ids = [aws_security_group.private.id]
   subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[0]
 
- user_data = <<EOF
+  user_data = <<EOF
   #!/bin/bah
   yum update -y
   yum install -y httpd git
@@ -92,6 +92,14 @@ resource "aws_security_group" "private" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
+  }
+
+  ingress {
+    description = "http from loab balancer"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.load_balancer.id]
   }
 
   egress {
